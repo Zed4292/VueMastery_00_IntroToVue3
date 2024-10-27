9f53fa6d9e57d85fc727b61ee6bd419f3f9306d5
Zed4292 <stephan.fuehrer92@gmail.com>
2024-10-27 21:09:14 +0100

    event handling

--- main.js
+++ main.js
@@ -12,10 +12,24 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants : [
-                {id: 2234, color: 'blue'},
-                {id: 2235, color: 'red'}
+                {id: 2234, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                {id: 2235, color: 'red', image: './assets/images/socks_red.jpg'}
             ],
-            sizes: ['39-42', '43-45', '46-48']
+            sizes: ['39-42', '43-45', '46-48'], 
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        delFromCart() {
+            if (this.cart > 0){
+                this.cart -= 1
+            }
         }
     }
 })
