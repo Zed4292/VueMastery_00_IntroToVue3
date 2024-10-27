792d0662039f1a7ac76cc34460eaa321ce470e75
Zed4292 <stephan.fuehrer92@gmail.com>
2024-10-27 18:30:45 +0100

    import some test data

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: {
+                name: 'Socks',
+                description: 'These are the best socks you have ever seen'
+            }
+        }
+    }
+})
