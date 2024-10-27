a406bd4f7020db30e104eac2b2e9627462fe5b30
Zed4292 <stephan.fuehrer92@gmail.com>
2024-10-27 19:42:06 +0100

    list rendering

--- main.js
+++ main.js
@@ -9,7 +9,13 @@ const app = Vue.createApp({
             url: 'https://google.com',
             inStock: true,
             inventory: 8,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants : [
+                {id: 2234, color: 'blue'},
+                {id: 2235, color: 'red'}
+            ],
+            sizes: ['39-42', '43-45', '46-48']
         }
     }
 })
